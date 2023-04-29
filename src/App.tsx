f99384829fb63cae85ad39bd2fb296caf7f20049
pkolt <pkoltyshev@gmail.com>
2023-04-29 22:36:41 +0400

    Fix components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
-const App = () => {
-  return (
-    <>
-      
-    </>
-  )
-}
+import { useState } from 'react';
+import './App.css';
 
-export default App
+const App = () => {
+  const [coffee, setCoffee] = useState(false);
+  const [tea, setTea] = useState(false);
+  return (
+    <div id="app">
+      <form>
+        <fieldset>
+          <legend>Menu</legend>
+          <p className="field">
+            <input
+              type="checkbox"
+              name="coffee"
+              id="id_coffee"
+              onChange={() => setCoffee((state) => !state)}
+              checked={coffee}
+            />
+            <label htmlFor="id_coffee">Coffee</label>
+          </p>
+          <p className="field">
+            <input type="checkbox" name="tea" id="id_tea" onChange={() => setTea((state) => !state)} checked={tea} />
+            <label htmlFor="id_tea">Tea</label>
+          </p>
+        </fieldset>
+      </form>
+
+      <p>
+        {coffee && (
+          <>
+            Selected Coffee
+            <br />
+          </>
+        )}
+        {tea && (
+          <>
+            Selected Tea
+            <br />
+          </>
+        )}
+      </p>
+    </div>
+  );
+};
+
+export default App;
