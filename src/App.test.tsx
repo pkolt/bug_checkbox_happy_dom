e94f6758d2377bd05655755ed5fd562db65a7c8f
pkolt <pkoltyshev@gmail.com>
2023-04-30 22:34:03 +0400

    Update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,27 +1,40 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
-describe('App', () => {
-  it('render', () => {
+describe('Test Checkbox', () => {
+  it('event onChange', async () => {
     const { container } = render(<App />);
-    expect(container).toMatchSnapshot();
-  });
-
-  it('select checkbox', async () => {
-    const { container } = render(<App />);
-    const checkbox = screen.getByTestId('tea');
+    const checkbox = screen.getByTestId('tea') as HTMLInputElement;
     await userEvent.click(checkbox);
-    screen.debug(container);
     expect(container.innerHTML).toMatch('Selected Tea');
   });
 
-  it('select radio button', async () => {
+  it('event change', async () => {
+    const handle = vi.fn();
+    render(<App />);
+    const checkbox = screen.getByTestId('tea') as HTMLInputElement;
+    checkbox.addEventListener('change', handle);
+    await userEvent.click(checkbox);
+    expect(handle).toHaveBeenCalled();
+  });
+});
+
+describe('Test Radio button', () => {
+  it('event onChange', async () => {
     const { container } = render(<App />);
     const radioBtn = screen.getByTestId('cookie');
     await userEvent.click(radioBtn);
-    screen.debug(container);
     expect(container.innerHTML).toMatch('Selected cookie');
+  });
+
+  it('event change', async () => {
+    const handle = vi.fn();
+    render(<App />);
+    const radioBtn = screen.getByTestId('cookie');
+    radioBtn.addEventListener('change', handle);
+    await userEvent.click(radioBtn);
+    expect(handle).toHaveBeenCalled();
   });
 });
